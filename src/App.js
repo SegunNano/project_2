ba7bfe63a9716f9bc288c69ccab9e93339ec1493
SegunNano <segunfadipe97@gmail.com>
2024-05-06 18:14:27 +0100

    Fancy todo list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import ColorBoxes from './components/ColorBox/ColorBoxes';
+// import ColorBoxes from './components/ColorBox/ColorBoxes';
+import Todo from "./components/Todo/Todo";
 
-const colors = ['#F6F5F2', '#F0EBE3', '#F3D0D7', '#FFEFEF', '#FFC94A', '#C08B5C', '#795458', '#453F78', '#0C0C0C', '#481E14', '#627254'];
+// const colors = ['#F6F5F2', '#F0EBE3', '#F3D0D7', '#FFEFEF', '#FFC94A', '#C08B5C', '#795458', '#453F78', '#0C0C0C', '#481E14', '#627254'];
+
 
 function App() {
+  //colorBox rendering
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <ColorBoxes colors={colors} />
+  //     </header>
+  //   </div>
+  // );
+
+
+  //TodoList Rendering
   return (
-    <div className="App">
-      <header className="App-header">
-        <ColorBoxes colors={colors} />
-      </header>
-    </div>
+    <Todo />
   );
 }
 
---
+++ src/components/Todo/Todo.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import { v4 as uuid } from "uuid";
+import './Todo.css';
+const Todo = () => {
+    const [todos, setTodos] = useState([]);
+    const [input, setInput] = useState('');
+    const handleSubmit = () => {
+        if (input) {
+            setTodos((todos) =>
+                todos = [...todos, { text: input, id: uuid() }]
+            );
+            setInput('');
+        }
+    };
+    const removeTodo = (id) => {
+        setTodos((todos) => todos.filter((t) => t.id !== id));
+    };
+    return (
+        <>
+            <div className="container">
+                <input type="text" value={input} onChange={(e) => setInput(e.target.value)} placeholder="New Todo" />
+                <button onClick={handleSubmit}>Submit</button>
+                <ul className="todos_list">
+                    {
+                        todos.map(({ text, id }) => {
+                            return <li onDoubleClick={() => removeTodo(id)} key={id} className="todo">
+                                <span>{text}</span>
+                            </li>;
+                        })
+                    }
+                </ul>
+            </div>
+        </>
+    );
+};
+
+export default Todo;
